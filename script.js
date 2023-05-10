72cc41075ecb2deeb25094e6a4f54016c24b4275
Napster <rome.1205h@gmail.com>
2023-05-09 23:43:57 -0500

    add elements

---
+++ script.js
@@ -0,0 +1,9 @@
+const apiKey = '3265874a2c77ae4a04bb96236a642d2f';
+
+const main = document.getElementById('main');
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+const url = (city) =>
+   `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
